1cfe54a2564ea985032f757a4f11b77a2d155085
TomoyaKinami <8921056@ed.tus.ac.jp>
2024-01-02 13:56:03 +0900

    comiit

--- routes/signup.js
+++ routes/signup.js
@@ -13,70 +13,93 @@ router.post("/", upload.fields([]), (req, res) => {
   const password = req.body.password;
   const received_csrfToken = req.body._csrf;
   const session_csrfToken = req.session.signup;
+  const recapcha = req.body.recaptcha;
   console.log(req.body);
   console.log(username);
   ///フォームから受け取ってきたcsrfトークンとsessionのcsrfトークンが一致しているか確認
   if (received_csrfToken == session_csrfToken) {
-    ///登録したいパスワードをハッシュ化
-    let hashed_password = bcrypt.hashSync(password, 10);
-    let db = new sqlite3.Database("DV.sqlite3");
-    // 既存のユーザーネームとの重複をチェック
-    db.all("SELECT * FROM users", function (err, row) {
-      if (err) {
-        console.error(err.message);
-        db.close();
-        return res.render("error.ejs", { code: "500" });
-      } else {
-        db.close(); ///必ず閉める
-        for (let i = 0; i < row.length; i++) {
-          // ユーザーネーム・パスワードが両方とも同じものがある場合は同じusernameとpswdは認められない。
-          if (row[i]["username"] == username) {
-            ///下のコードの意味:平文の登録したいpasswordとデータベース上のハッシュ値パスワードを比較
-            if (bcrypt.compareSync(password, row[i]["password"]) == true) {
+    ///リキャプチャ確認
+    // 1. application/x-www-form-urlencoded形式でPOSTする
+
+    // 2. URLSearchParamsクラスのインスタンスを作成する
+    const params = new URLSearchParams();
+    params.append("secret", "6Lf-IhEpAAAAALKCfDqeCXcUyjhaHqNFOA328CoV");
+    params.append("response", recapcha);
+
+    // 3. Post通信をする
+    const postResponse = async () => {
+      const response = await fetch(
+        "https://www.google.com/recaptcha/api/siteverify",
+        {
+          method: "POST", // HTTP-Methodを指定する！
+          body: params, // リクエストボディーにURLSearchParamsを設定
+        }
+      );
+      const data = await response.json();
+      return data;
+    };
+
+    postResponse().then((data) => {
+      console.log(data);
+
+      ///登録したいパスワードをハッシュ化
+      let hashed_password = bcrypt.hashSync(password, 10);
+      let db = new sqlite3.Database("DV.sqlite3");
+      // 既存のユーザーネームとの重複をチェック
+      db.all("SELECT * FROM users", function (err, row) {
+        if (err) {
+          console.error(err.message);
+          db.close();
+          return res.render("error.ejs", { code: "500" });
+        } else {
+          db.close(); ///必ず閉める
+          for (let i = 0; i < row.length; i++) {
+            // ユーザーネーム同じものがある場合は同じusernameは認められない。
+            if (row[i]["username"] == username) {
               var pswd_error = true;
               break;
             }
           }
-        }
 
-        ///ユーザーネーム・パスワードが両方とも同じものがある場合
-        if (pswd_error) {
-          const csrfToken = pswd_js.createPassword();
-          req.session.signup = csrfToken;
-          ///csrfトークンを生成し直して、もう一回新しいpswdにするよう画面に表示させる。
+          ///ユーザーネームが同じものがある場合
+          if (pswd_error) {
+            const csrfToken = pswd_js.createPassword();
+            req.session.signup = csrfToken;
+            ///csrfトークンを生成し直して、もう一回新しいにするよう画面に表示させる。
 
-          var return_list = [csrfToken, false];
-          res.send(return_list);
-        } else {
-          ///ユーザーネーム・パスワードが両方とも同じものがない場合
-          // ユーザーの作成やデータベースへの保存などの処理を実装する
-          db = new sqlite3.Database("DV.sqlite3");
-          db.serialize(() => {
-            var authorization = authorization_js.create_authorization(
-              "users",
-              db,
-              "authorization"
-            );
-            db.run(
-              "INSERT INTO users (username, password, authorization) VALUES (?, ?, ?)",
-              [username, hashed_password, authorization],
-              (err) => {
-                if (err) {
-                  console.error(err.message);
-                  db.close();
-                  return res.render("error.ejs", { code: "500" });
+            var return_list = [csrfToken, false];
+            res.send(return_list);
+          } else {
+            ///ユーザーネーム・パスワードが両方とも同じものがない場合
+            // ユーザーの作成やデータベースへの保存などの処理を実装する
+            db = new sqlite3.Database("DV.sqlite3");
+            db.serialize(() => {
+              var authorization = authorization_js.create_authorization(
+                "users",
+                db,
+                "authorization"
+              );
+              db.run(
+                "INSERT INTO users (username, password, authorization) VALUES (?, ?, ?)",
+                [username, hashed_password, authorization],
+                (err) => {
+                  if (err) {
+                    console.error(err.message);
+                    db.close();
+                    return res.render("error.ejs", { code: "500" });
+                  }
                 }
-              }
-            );
-            db.close(); ///必ず閉める
-          });
-          ///適切に変更できたという表示をつけて、ログイン画面にリダイレクト。
-          const csrfToken = pswd_js.createPassword();
-          req.session.signup = csrfToken;
-          var return_list = [csrfToken, true];
-          res.send(return_list);
+              );
+              db.close(); ///必ず閉める
+            });
+            ///適切に変更できたという表示をつけて、ログイン画面にリダイレクト。
+            const csrfToken = pswd_js.createPassword();
+            req.session.signup = csrfToken;
+            var return_list = [csrfToken, true];
+            res.send(return_list);
+          }
         }
-      }
+      });
     });
     ///フォームから受け取ってきたcsrfトークンとsessionのcsrfトークンが一致していない場合
   } else {
